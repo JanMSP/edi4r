01dbaf3b336616fea1bfde7ccfcbf9d78f16f858
Jan Petersen <jp@novitas.dk>
2020-01-10 13:13:14 +0100

    fix encoding error

--- lib/edi4r/edifact.rb
+++ lib/edi4r/edifact.rb
@@ -64,8 +64,8 @@ module EDI::E
   # Use pattern for allowed chars of UNOC charset if none given explicitly
   #
   Illegal_Charset_Patterns = Hash.new(/[^-A-Za-z0-9 .,()\/=!%"&*;<>'+:?\xa0-\xff]+/n)
-  Illegal_Charset_Patterns['UNOA'] =     /[^-A-Z0-9 .,()\/=!%"&*;<>'+:?]+/
-  Illegal_Charset_Patterns['UNOB'] =  /[^-A-Za-z0-9 .,()\/=!%"&*;<>'+:?]+/
+  Illegal_Charset_Patterns['UNOA'] =     /[^-A-Z0-9 .,()\/=!%"&*;<>'+:?]+/u
+  Illegal_Charset_Patterns['UNOB'] =  /[^-A-Za-z0-9 .,()\/=!%"&*;<>'+:?]+/u
   Illegal_Charset_Patterns['UNOW'] =  /[^-A-Za-z0-9 .,()\/=!%"&*;<>'+:?\u{00}-\u{10ffff}}]+/
   # more to come...
 
